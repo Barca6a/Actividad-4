935e3ebb8a65ed0f9e58f99042753a8447a8fc45
Barca6a <a388563@uach.mx>
2026-09-15 08:49:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@
 if(temperatura<10):st.write("Hace frio")
   elif(temperatura>=10 and temperatura <=24):st.write("la temperatura es agradable)
   else:st.write("hace calor")
-  temperatura= int(input("Temperatura en grados celsius:"))
+temperatura=st.write(10)
